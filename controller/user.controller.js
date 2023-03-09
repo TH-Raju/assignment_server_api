5371030f47a88c829eeed99028b76a9c8c71851f
TH-Raju <rajukhan894200@gmail.com>
2023-03-09 18:31:54 +0600

    CRUD operation done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,20 +4,37 @@ module.exports.getAllUser = (req, res) => {
     res.send(user);
 };
 
+
+/*
+--------------------
+    To get a single user
+------------
+*/
 module.exports.getAUser = (req, res) => {
     const { id } = req.params;
     const foundUser = user.find(usr => usr.id == id);
     res.send(foundUser);
 }
 
+/*
+--------------------
+    for save a new user in data
+------------
+*/
+
 module.exports.saveAUser = (req, res) => {
     user.push(req.body);
     res.send(user);
 }
 
+/*
+--------------------
+    To update a single user
+------------
+*/
+
 module.exports.updateUser = (req, res) => {
     const { id } = req.params;
-    // const filter = { _id: id };
     const newUser = user.find(usr => usr.id === Number(id));
     newUser.id = id;
     newUser.gender = req.body.gender;
@@ -29,10 +46,15 @@ module.exports.updateUser = (req, res) => {
 
 }
 
+/*
+--------------------
+    To Delete a  user
+--------------------
+*/
+
 module.exports.deleteUser = (req, res) => {
     const { id } = req.params;
     const filter = { _id: id };
-
     newuser = user.filter(usr => usr.id != Number(id));
     res.send(newuser);
 }
